a91eea6f785990e1a82cabaa97758d63d18c59fd
Proodent <proodent.it@gmail.com>
2025-10-06 11:17:42 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+
 import express from 'express';
 import { GoogleSpreadsheet } from 'google-spreadsheet';
 import { JWT } from 'google-auth-library';
